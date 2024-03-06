30afccb7ea4891c6884dedee7b50d7e64753ee92
Benjamin Peña <144568088+BenjaminCoderSW@users.noreply.github.com>
2024-03-06 16:02:42 -0600

    Hice otro pequeño cambio en la ruta, (cambie el nombre a cine)

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://localhost:90/codeIgniter/public';
+    public string $baseURL = 'http://localhost:90/cine/public';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
