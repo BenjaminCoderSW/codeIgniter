c2f6c0f2697667b931617cb9d73d8f4742510146
vaniaadonaji <vaniadonajitorres@gmail.com>
2024-03-11 09:19:28 -0600

    No eliminar un usuario, en este caso Tigger

--- app/Views/template/piepagina.php
+++ app/Views/template/piepagina.php
@@ -1,12 +1,3 @@
 </div>
 </body>
-<script>
-    function confirmarEliminacion() {
-        if (confirm("¿Estás seguro de que deseas eliminar este usuario?")) {
-            return true; 
-        } else {
-            return false; 
-        }
-    }
-</script>
 </html>
