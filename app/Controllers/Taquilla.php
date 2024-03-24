7e1c4d900e499916f5cf3b5294710800c56672ad
vaniaadonaji <vaniadonajitorres@gmail.com>
2024-03-24 14:43:54 -0600

    Vista de la taquilla comenzada

--- app/Controllers/Taquilla.php
+++ app/Controllers/Taquilla.php
@@ -6,6 +6,8 @@ use CodeIgniter\Controller;
 class Taquilla extends Controller{
     public function index()
     {
-        return view('taquilla/principal_taquilla');
+        $datos['cabecera'] = view('template/cabecera_taquilla');
+        $datos['piepagina'] = view('template/piepagina');
+        return view('taquilla/principal_taquilla',$datos);
     }
 }
--- app/Views/taquilla/principal_taquilla.php
+++ app/Views/taquilla/principal_taquilla.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BenVani Cineplex</title>
-</head>
-<body>
-    Holaaaaaaaaaaaaa, esta va a ser la taquilla
-</body>
-</html>
+<?=$cabecera?>
+
+<?=$piepagina?>
