228e99b51c600452501d377921b0f950362ac894
vaniaadonaji <vaniadonajitorres@gmail.com>
2024-03-24 22:36:12 -0600

    Comentarios en los controladores
    Se hicieron los comentarios necesarios en el codigo para documentar y comentar todo lo que fueron los controladores de: Peliculas, Home, Horarios, Login y Taquilla, solo falto el de Usuarios

--- app/Controllers/Horarios.php
+++ app/Controllers/Horarios.php
@@ -2,6 +2,7 @@
 namespace App\Controllers;
 
 use CodeIgniter\Controller;
+// Controlador de Horarios
 use App\Models\Horario;
 class Horarios extends Controller{
 
--- app/Controllers/Taquilla.php
+++ app/Controllers/Taquilla.php
@@ -2,12 +2,16 @@
 namespace App\Controllers;
 
 use CodeIgniter\Controller;
+// Controlador de Taquilla
 
 class Taquilla extends Controller{
+    //metodo de la vista principal
     public function index()
     {
+        //hacemo sun arreglo para mandarle datos a la vista
         $datos['cabecera'] = view('template/cabecera_taquilla');
         $datos['piepagina'] = view('template/piepagina');
+        //retornamos la vista y mandamos el arreglo
         return view('taquilla/principal_taquilla',$datos);
     }
 }
